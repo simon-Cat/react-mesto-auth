595f7880feaad5aeaef5d3fdb3b51ce49a063f13
GregoMur <GregoMur@yandex.ru>
2023-03-31 15:06:12 +0300

    feat: add Link into Header

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ import Login from "./Login";
 import Register from "./Register";
 import ProtectedRoute from "./ProtectedRoute";
 import InfoTooltip from "./InfoTooltip";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 
 const App = () => {
   // states
@@ -25,6 +25,8 @@ const App = () => {
   const [currentUser, setCurrentUser] = useState({});
   const [cards, setCards] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  const location = useLocation();
   
   // effect
   useEffect(() => {
@@ -146,7 +148,7 @@ const App = () => {
   return (
     <div className="page">
       <CurrentUserContext.Provider value={currentUser}>
-        <Header />
+        <Header isLoggedIn={isLoggedIn} location={location.pathname} />
         <Routes>
           <Route
             path="*"
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,33 @@
-import logo from '../images/logo.svg';
+import logo from "../images/logo.svg";
+import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ isLoggedIn, location }) => {
   return (
     <header className="page__header header">
       <a href="https://simon-cat.github.io/mesto/" className="header__link">
         <img src={logo} alt="Логотип" className="header__logo logo" />
       </a>
+      {isLoggedIn ? (
+        <div className="header__user-controll-block">
+          <span className="header__text">Gregomur@yandex.ru</span>
+          <Link to="/sing-in" replace={true} className="header__link">
+            Выйти
+          </Link>
+        </div>
+      ) : (
+        <div className="header__user-controll-block">
+          {(!isLoggedIn && (location === "/sing-in")) && (
+            <Link to="/sing-up" replace={true} className="header__link">
+              Регистрация
+            </Link>
+          )}
+          {(!isLoggedIn && location === "/sing-up") && (
+            <Link to="/sing-in" replace={true} className="header__link">
+              Войти
+            </Link>
+          )}
+        </div>
+      )}
     </header>
   );
 };
